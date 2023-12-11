8bcd523856dda1fc9cc4731eff9ba6792e9e275b
Daniel Escanez-Exposito <jdanielescanez@gmail.com>
2023-12-11 17:50:13 +0000

    Day 8.2

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,6 +1,7 @@
 
 use std::fs;
 use std::collections::HashMap;
+use num::integer;
 
 #[derive(Debug)]
 struct Journey {
@@ -25,28 +26,27 @@ impl Journey {
         Journey {instructions: instructions.to_string(), network}
     }
 
-    fn run(&self) -> u32 {
+    fn run(&self) -> u64 {
         let mut current_nodes = self.network.keys()
             .filter(|x| x.chars().collect::<Vec<char>>()[2] == 'A')
             .map(|x| x.clone())
             .collect::<Vec<String>>();
-        let mut counter: u32 = 0;
-        loop {
+        let mut counter: Vec<u64> = vec![0; current_nodes.len()];
+        let mut i: usize = 0;
+        while i < current_nodes.len() {
             for instruction in self.instructions.chars() {
-                for i in 0..current_nodes.len() {
-                    current_nodes[i] = match instruction {
-                        'L' => self.network[&current_nodes[i]].0.clone(),
-                        'R' => self.network[&current_nodes[i]].1.clone(),
-                        _ => panic!("{} is not a valid instruction", current_nodes[i]),
-                    };
+                current_nodes[i] = match instruction {
+                    'L' => self.network[&current_nodes[i]].0.clone(),
+                    'R' => self.network[&current_nodes[i]].1.clone(),
+                    _ => panic!("{} is not a valid instruction", current_nodes[i]),
+                };
+                counter[i] += 1;
+                if current_nodes[i].chars().collect::<Vec<char>>()[2] == 'Z' {
+                    i += 1;
                 }
             }
-            counter += self.instructions.len() as u32;
-            if current_nodes.iter().all(|x| x.chars().collect::<Vec<char>>()[2] == 'Z') {
-                break;
-            }
         }
-        counter
+        counter.iter().fold(1, |acc, number| integer::lcm(acc, *number))
     }
 }
 
